032746e74446aaefab47afd82f61460ee0acbfd0
Fahad Israr <fahad00cms@gmail.com>
2019-07-05 23:11:57 +0530

    Updates

---
+++ precache-manifest.1a5e489c307758231854ff7ac050d566.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "905181f9962d6f0baa7c19e4a48463c7",
+    "url": "/profile/index.html"
+  },
+  {
+    "revision": "d6730507051597aadfa7",
+    "url": "/profile/static/css/2.38750c1c.chunk.css"
+  },
+  {
+    "revision": "ddbb0b937792457983a7",
+    "url": "/profile/static/css/main.9ff42c90.chunk.css"
+  },
+  {
+    "revision": "d6730507051597aadfa7",
+    "url": "/profile/static/js/2.0e3a9d19.chunk.js"
+  },
+  {
+    "revision": "ddbb0b937792457983a7",
+    "url": "/profile/static/js/main.fef66bdc.chunk.js"
+  },
+  {
+    "revision": "189fd871db148202566e",
+    "url": "/profile/static/js/runtime~main.cc8686f1.js"
+  },
+  {
+    "revision": "7f65a5cd8a505c0285572827c75e5c8b",
+    "url": "/profile/static/media/Resume_Fahad_Israr_May_2019.7f65a5cd.pdf"
+  },
+  {
+    "revision": "5e8b7aa4e712c0461dc39cf27547f512",
+    "url": "/profile/static/media/cat.5e8b7aa4.png"
+  },
+  {
+    "revision": "14de5a904443705688e31dee3ced85bb",
+    "url": "/profile/static/media/drupal.14de5a90.png"
+  },
+  {
+    "revision": "1be42f20c329d1ac2480553255174912",
+    "url": "/profile/static/media/ecell.1be42f20.png"
+  },
+  {
+    "revision": "8f23430f794409796f0c829877f9c8da",
+    "url": "/profile/static/media/face.8f23430f.png"
+  },
+  {
+    "revision": "f99275767eca4982690b214cd8bd228b",
+    "url": "/profile/static/media/library.f9927576.png"
+  },
+  {
+    "revision": "549742dc4780d04a4ff8576360d75de9",
+    "url": "/profile/static/media/postgresql.549742dc.png"
+  },
+  {
+    "revision": "64c769ee095b1775665ee13e291b49b5",
+    "url": "/profile/static/media/techfest.64c769ee.png"
+  },
+  {
+    "revision": "1b1a5e861f89ec90bd2530a14644e7d0",
+    "url": "/profile/static/media/tp.1b1a5e86.png"
+  }
+]);
